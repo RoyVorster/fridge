c60520d80932b9407473341fccbe89bb2c196fcd
Roy Vorster <royvorster@gmail.com>
2021-02-24 21:58:49 +0100

    Constant rate retrieve new temperature

--- fridge/main/fridge.cpp
+++ fridge/main/fridge.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 #include "esp_system.h"
 
 #include "mcp9808.h"
-
 
 extern "C" {
     void app_main(void);
@@ -12,10 +10,17 @@ extern "C" {
 void app_main(void) {
     setvbuf(stdout, NULL, _IONBF, 0);
 
+    // Init sensor
     Sensor sensor = Sensor();
     esp_err_t ret = sensor.initialize();
     ESP_ERROR_CHECK(ret);
 
-    float temp = sensor.get_temperature();
-    printf("Temperature: %f\n", temp);
+    // Retrieve data
+    float temp = 0;
+    while (true) {
+        temp = sensor.get_temperature();
+        printf("Temperature: %f\n", temp);
+
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
 }
