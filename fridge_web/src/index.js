607acdd890ab5d55ca369d37fd9736886ceb2e4b
Roy Vorster <royvorster@gmail.com>
2021-03-01 13:11:01 +0100

    Simple socket io based implementation

--- fridge_web/src/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-   let element = document.createElement('div');
-   element.innerHTML = 'Hello, world!';
-
-   return element;
-}
-
-document.body.appendChild(component());
-
